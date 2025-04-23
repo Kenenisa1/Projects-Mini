e05692716c2e7f0a0092bacac4ea436b92eea4dd
Kenenisa <kenenisamiesob.com>
2025-04-23 09:56:12 +0300

    practicing how to response to event on App.jsx component, using button click

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,16 +1,26 @@
 import reactLogo from './assets/react.svg'
 
 function App() {
+
   const List=[
     {name: "Mac" , IsmyPc: false, id: 1},
     {name: "Windonws" , Ismypc: true, id: 2},
     {name: "Linux" , Ismypc: false, id: 3}
   ]
+
+  function Events()
+  {
+    const hw =document.getElementById("he");
+    hw.innerHTML= "Why are you clicking me"
+  }
+
 const OsList=List.map(item => 
        <li key={item.id} style={{
-        color: item.IsmyPc ? "Green": "blue"
-       }}> {item.name} {item.IsmyPc}</li>
+            color: item.IsmyPc ? "Green": "blue"}}> 
+          {item.name} {item.IsmyPc}
+       </li>
     )
+
   return (
     <>
       <div>
@@ -21,6 +31,10 @@ const OsList=List.map(item =>
       <ul>
           {OsList}
       </ul>
+      <button onClick={Events}>
+        don't Click me
+      </button>
+      <h1 id='he'></h1>
     </>
   )
 }
